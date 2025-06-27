5b1b9175c6745e0171cb11f1102bc33b1a0ee57e
Bhavesh Soni <bhaveshsoni.py@gmail.com>
2025-06-27 09:26:56 +0530

    Update Graph_Plotter.py

--- Graph_Plotter/Graph_Plotter.py
+++ Graph_Plotter/Graph_Plotter.py
@@ -1,31 +1,14 @@
-# import matplotlib.pyplot as plt
-
-# x = [2, 4, 5]
-# y = [2, 3, 6]
-
-# plt.plot(x, y)
-
-
-# plt.xlabel('X Axis')
-
-# plt.ylabel( 'Y Axis')
-
-# plt.title('Demo Graph ')
-
-# plt.show()
-
-
 import matplotlib.pyplot as plt
 
-# Take input from the user for x and y values
+Take input from the user for x and y values
 x_input = input("Enter X values separated by spaces: ")
 y_input = input("Enter Y values separated by spaces: ")
 
-# Convert the input strings to lists of integers
+Convert the input strings to lists of integers
 x = list(map(float, x_input.strip().split()))
 y = list(map(float, y_input.strip().split()))
 
-# Check if both lists are the same length
+
 if len(x) != len(y):
     print("Error: The number of X and Y values must be the same.")
 else:
